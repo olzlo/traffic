f4d4c8d0fe4280e890b7cee823dcd577945163f0
cyzkul@gmail.com <cyzkul@gmail.com>
2019-04-30 17:43:00 +0800

    add auth from env

--- cmd/go.mod
+++ cmd/go.mod
@@ -1,3 +1,3 @@
 module traffic
 
-require github.com/zsichen/traffic v0.0.0-20190430083231-8a0d4592621d
+require github.com/zsichen/traffic v0.0.0-20190430083554-ea051da7afc3
--- src/auth.go
+++ src/auth.go
@@ -1,12 +1,42 @@
 package src
 
-type Auth interface {
-	initPasswd() string
+import (
+	"os"
+	"strings"
+)
+
+//Auth user authentication
+type IAuth interface {
+	init()
+	//pre-shared key encrypted handshake
+	SharedKey() (string, bool)
+	User(string) (string, bool)
 }
 
-func NewAuthWithRds() *Auth {
+//NewAuthWithRds from redis
+func NewAuthWithRds() *IAuth {
+	return nil
+}
+
+//NewAuthWithEnv from env
+func NewAuthWithEnv() *IAuth {
+	return nil
+}
+
+var _ IAuth = &env{}
+
+type env struct {
+}
+
+func (e *env) init() {
 
 }
-func NewAuthWithEnv() *Auth {
 
+func (e *env) SharedKey() (key string, ok bool) {
+	key, ok = os.LookupEnv("TRAFFIC_SHARED")
+	return
+}
+func (e *env) User(uname string) (pwd string, ok bool) {
+	pwd, ok = os.LookupEnv("TRAFFIC_USER_" + strings.ToUpper(uname))
+	return
 }
